ceb424a4cf13a9256ef719dc100dd2c3c57e5829
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-01-28 11:04:13 -0500

    last changes

--- public/js/CMS.js
+++ public/js/CMS.js
@@ -1,6 +1,6 @@
 function CMS() {
   'use strict';
-
+  var maxTweets = 30;
   var self = this;
 
   this.createTwitterPanels = function(opts) {
@@ -8,28 +8,44 @@ function CMS() {
     var users = opts.users || [];
     var dragFrom, dragTo;
 
-    this.panels = [];
+    var panels = [];
+    var _onReorder = function() {};
+
+    function swapElem(moving, bumped) {
+      moving.parentNode.replaceChild(moving, bumped);
+      moving.parentNode.insertBefore(bumped, moving);
+
+      var list = container.getElementsByTagName('tweets-panel');
+      for (var i=0; i < list; i++) {
+        console.log(list[i].user);
+      }
+
+      _onReorder(users);
+    }
 
     users.forEach(function(user, index) {
       var panel = new TwitterPanel(user);
+      panel.innerHTML = 'Loading the ' + maxTweets + ' most recent tweets from @' + user + '...';
       panel.index = index;
-      self.panels.push(panel);
-      container.appendChild(panel);
 
       panel.addEventListener('dragstart', function panelDrag(e) {
-        dragFrom = e.target.index;
+        dragFrom = e.target;
       });
-
       panel.addEventListener('dragenter', function panelMove(e) {
-        dragTo = e.target.index;
+        dragTo = e.target;
+        swapElem(dragFrom, dragTo);
       });
-
       panel.addEventListener('dragend', function panelDrop(e) {
-        
-        console.log('dragend', dragFrom, dragTo);
+        swapElem(dragFrom, dragTo);
       });
-
+      container.appendChild(panel);
     });
+
+    return {
+      onReorder: function(callback) {
+        _onReorder = callback;
+      }
+    };
 
   };
 
